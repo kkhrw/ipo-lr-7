ac1434815bba4b127f3a4d073bf6272c9e4999e3
uljana.korolenko@gmail.com <uljana.korolenko@gmail.com>
2025-12-09 19:15:31 +0300

    Выполнла задание2

--- task_2/main.py
+++ task_2/main.py
@@ -1,28 +1,25 @@
+#Козлова 1 вариант
+print("start code ...")
 import json
-print("start code")
-file = "dump.json"
-num = str(input("Введите необходимый код  квалификации"))
-skills = False
-with open(file, "r",encoding = "UTF-8") as file:
-    data = json.load(file)
-    for skill in data:
-        if skill["model"] == "data.skill":
-            skill_code = skill["fields"]["code"]
-            skill_title = skill["fields"]["title"]
-            skills = True
+with open("dump.json","r",encoding="utf-8") as file:
+    data=json.load(file)
+    skills=[]
+    for item in data:
+        if item.get("model")=="data.skill":
+            skills.append({
+            "code":item["fields"]["code"],
+            "title":item["fields"]["title"]
+        })
+user_str=input("Введите номер квалификации: ").strip()
 
-        for profession in data:
-            if profession["model"]=="data.speciality":
-                code_title = profession["fields"]["code"]
-                if code_title in num:
-                    prof_title = profession["fields"]["title"]
-                    c_type_title = profession["fields"]["c_type"]    
-        break
-
-if not skills:
-   print("=============== Не найдено ===============")   
+found=[]
+for skill in skills:
+    if user_str == skill["code"]:
+        found.append(skill)
+if found:
+    print("=" * 20 + "Найдено" + "=" * 20)
+    for skill in found:
+        print(f'{skill["code"]} >> Специальность "{skill["title"]}"')
 else:
-    print("=============== Найдено ===============")
-    print(f" {code_title} >> Специальность {prof_title}, {c_type_title}")
-    print(f" {skill_code} >> Квалификация {skill_title}")
-print("end code")    
+            print("=" * 20 + "Не найдено" + "=" * 20)
+print("... end code")
